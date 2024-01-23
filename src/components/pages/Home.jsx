f8cf3d1b5da514887d34c0d34a8cb359d91a684b
GhostMobin <s1810677110@ru.ac.bd>
2024-01-23 21:34:22 +0600

    nav, hero, login

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hero from '../shared/Hero';
+
+const Home = () => {
+    return (
+        <div>
+            <Hero></Hero>
+
+        </div>
+    );
+};
+
+export default Home;
