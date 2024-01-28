38e74556fe553f225089d8bbe232ac6cd3eeb001
GhostMobin <s1810677110@ru.ac.bd>
2024-01-28 23:14:30 +0600

    hero fix

--- src/components/shared/Hero.jsx
+++ src/components/shared/Hero.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
     return (
-        <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
+        <div className="hero min-h-screen bg-top" style={{ backgroundImage: 'url("https://source.unsplash.com/L5nd7rPrEic")' }}>
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                <div className="flex flex-col items-center">
+                    <h1 className="mb-5 text-5xl font-bold max-w-lg">Discover Your Dream Home with Ease</h1>
+                    <p className="mb-5 max-w-2xl">Streamlining the Rental Experience for Homeowners and Renters Alike. List, Search, and Book Your Perfect Home Hassle-Free!</p>
+                    <Link to='/signup' className="btn btn-primary">Get Started</Link>
                 </div>
             </div>
         </div>
